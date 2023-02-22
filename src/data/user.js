86f648d18ef3734efcd1dd902c6188086efdd084
Gabriela Galvan <gabriela1galvan@gmail.com>
2023-02-21 17:24:56 -0700

    completed lab

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,7 @@
-const username = "Liza";
-const city = "New York";
+const username = "Gaby";
+const city = "Tucson";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
+export { city, username, image};
